bb59662ac33df06d7206f577972162f2c63abe53
José Antonio Gallo Junior <gallojunior@gmail.com>
2023-03-16 10:28:07 -0300

    Projeto Concluido

--- Pokedex/Services/PokeService.cs
+++ Pokedex/Services/PokeService.cs
@@ -5,6 +5,8 @@ namespace Pokedex.Services;
 public class PokeService : IPokeService
 {
     private readonly IHttpContextAccessor _session;
+    private readonly string pokemonFile = @"Data\pokemons.json";
+    private readonly string tiposFile = @"Data\tipos.json";
 
     public PokeService(IHttpContextAccessor session)
     {
@@ -12,46 +14,75 @@ public class PokeService : IPokeService
         PopularSessao();
     }
 
-    public Pokemon GetPokemon(int Numero)
-    {
-        PopularSessao();
-        
-        var pokemons = JsonSerializer.Deserialize<List<Pokemon>>(
-            LerArquivo(@"Data\pokemons.json")
-        );
-
-        return null;
-    }
-
     public List<Pokemon> GetPokemons()
     {
-        return null;
+        PopularSessao();
+        var pokemons = JsonSerializer.Deserialize<List<Pokemon>>
+            (_session.HttpContext.Session.GetString("Pokemons"));
+        return pokemons;
     }
 
     public List<Tipo> GetTipos()
     {
-        var tipos = JsonSerializer.Deserialize<List<Tipo>>(
-            LerArquivo(@"Data\tipos.json")
-        );
-        return null;
+        PopularSessao();
+        var tipos = JsonSerializer.Deserialize<List<Tipo>>
+            (_session.HttpContext.Session.GetString("Tipos"));
+        return tipos;
+    }
+
+    public Pokemon GetPokemon(int Numero)
+    {
+        var pokemons = GetPokemons();
+        return pokemons.Where(p => p.Numero == Numero).FirstOrDefault();
+    }
+
+    public PokedexDto GetPokedexDto()
+    {
+        var pokes = new PokedexDto()
+        {
+            Pokemons = GetPokemons(),
+            Tipos = GetTipos()
+        };
+        return pokes;
+    }
+
+    public DetailsDto GetDetailedPokemon(int Numero)
+    {
+        var pokemons = GetPokemons();
+        var poke = new DetailsDto()
+        {
+            Current = pokemons.Where(p => p.Numero == Numero)
+                .FirstOrDefault(),
+            Prior = pokemons.OrderByDescending(p => p.Numero)
+                .FirstOrDefault(p => p.Numero < Numero),
+            Next = pokemons.OrderBy(p => p.Numero)
+                .FirstOrDefault(p => p.Numero > Numero),
+        };
+        poke.Tipos = GetTipos();
+        return poke;
+    }
+
+    public Tipo GetTipo(string Nome)
+    {
+        var tipos = GetTipos();
+        return tipos.Where(t => t.Nome == Nome).FirstOrDefault();
     }
 
     private void PopularSessao()
     {
         if (string.IsNullOrEmpty(_session.HttpContext.Session.GetString("Tipos")))
         {
-            _session.HttpContext.Session.SetString("Tipos", LerArquivo(@"Data\tipos.json"));
-            _session.HttpContext.Session.SetString("Pokemons", LerArquivo(@"Data\pokemons.json"));
+            _session.HttpContext.Session.SetString("Pokemons", LerArquivo(pokemonFile));
+            _session.HttpContext.Session.SetString("Tipos", LerArquivo(tiposFile));
         }
     }
 
-    private string LerArquivo(string nomeArquivo)
+    private string LerArquivo(string fileName)
     {
-        using (StreamReader leitor = new StreamReader(nomeArquivo))
+        using (StreamReader leitor = new StreamReader(fileName))
         {
             string dados = leitor.ReadToEnd();
             return dados;
         }
-    }    
-
+    }
 }
